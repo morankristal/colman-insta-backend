e7e88f4707381946968a6a02ca72b7d2cb0e74e7
ronenanuka6 <ronenanuka6@gmail.com>
2025-01-25 16:11:37 +0200

    added login with google

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -9,6 +9,7 @@ export interface IUser extends Document {
     _id: string;
     refreshToken: string[];
     profilePicture: string;
+    googleId?: string;
 }
 
 const userSchema = new Schema<IUser>({
@@ -28,7 +29,13 @@ const userSchema = new Schema<IUser>({
     },
     password: {
         type: String,
-        required: true,
+        required: function (this: IUser) { return !this.googleId; },
+        validate: {
+            validator: function (v: string) {
+                return !!this.googleId || v.length > 0;
+            },
+            message: "Password is required"
+        },
     },
     refreshToken: {
         type: [String],
@@ -37,6 +44,10 @@ const userSchema = new Schema<IUser>({
     profilePicture: {
         type: String,
         default: '',
+    },
+    googleId: {
+        type: String,
+        unique: true,
     },
 });
 
