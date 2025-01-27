46504d3f8a6908387f07dce0aece9bdc4b2ab54c
ronenanuka6 <ronenanuka6@gmail.com>
2025-01-27 22:10:20 +0200

    added ai

--- src/controllers/ask.controller.ts
+++ src/controllers/ask.controller.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
-import axios from "axios";
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apikey = process.env.GOOGLE_API_KEY as string;
 
 class AskController {
     async askQuestion(req: Request, res: Response) {
         try {
-            const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+            const genAI = new GoogleGenerativeAI(apikey);
             const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
             const prompt = req.body.prompt;
+
             const result = await model.generateContent(prompt);
-            console.log(result.response.text());
             res.status(200).send(result.response.text());
         } catch (error) {
             console.error(error);
--- src/routes/ask.router.ts
+++ src/routes/ask.router.ts
@@ -6,9 +6,20 @@ const router = Router();
 /**
  * @swagger
  * /ai:
- *   get:
+ *   post:
  *     summary: Ask a question to AI
- *     description: Sends a question to the AI and retrieves the response.
+ *     description: Sends a question in the request body to the AI and retrieves the response.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               prompt:
+ *                 type: string
+ *                 description: The question to ask the AI.
+ *                 example: "What is the best city in Israel"
  *     responses:
  *       200:
  *         description: AI response retrieved successfully.
@@ -19,11 +30,13 @@ const router = Router();
  *               properties:
  *                 reply:
  *                   type: string
- *                   example: "This is the AI's response."
+ *                   example: "Netanya is the best city in Israel."
+ *       400:
+ *         description: Bad request, prompt is required.
  *       500:
  *         description: Internal server error.
  */
-router.get("/ai", askController.askQuestion);
+router.post("/ai", askController.askQuestion);
 
 
 export default router;
