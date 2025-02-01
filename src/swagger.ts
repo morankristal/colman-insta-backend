82d7e1317d0d7b60148aa4c9cbc9af62fbbf5fef
ronenanuka6 <ronenanuka6@gmail.com>
2025-02-01 15:23:37 +0200

    fix

--- src/swagger.ts
+++ src/swagger.ts
@@ -10,7 +10,7 @@ const swaggerOptions: Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: process.env.BASE_URL || "http://localhost:3000",
       },
     ],
     components: {
